b684c4e0b6d77cfe42555638f83f6c7b8a1940b9
Joseph M <jmedina1985@hotmail.com>
2021-04-02 21:25:18 -0700

    set up About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,13 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus
+        doloribus odit ea modi recusandae, veniam exercitationem ab enim, animi
+        eaque blanditiis earum, eveniet nostrum ipsum. Voluptates reprehenderit
+        soluta esse, fugiat eligendi numquam totam quae amet eos, consectetur
+        tenetur deleniti.
+      </p>
     </section>
   );
 };
