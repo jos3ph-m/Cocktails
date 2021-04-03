77a0acb083823b42458ceb21bdc0a0a1e83f3ccc
Joseph M <jmedina1985@hotmail.com>
2021-04-02 21:22:44 -0700

    working on Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,14 @@ const Navbar = () => {
         <Link to="/">
           <img src={logo} alt="the cocktail db" className="logo"></img>
         </Link>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
