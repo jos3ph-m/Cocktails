65bb7edd321e14655772ae57a39bff29d4ef3336
Joseph M <jmedina1985@hotmail.com>
2021-04-03 21:01:58 -0700

    Context API setup

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -7,6 +7,14 @@ const CocktailList = () => {
   const { cocktails, loading } = useGlobalContext();
 
   if (loading) {
+    return <Loading />;
+  }
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">
+        no cocktails matched your search criteria
+      </h2>
+    );
   }
   return (
     <div>
