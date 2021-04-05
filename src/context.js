3b0007480b075e347214aad39f960bb6c20b5f37
Joseph M <jmedina1985@hotmail.com>
2021-04-04 21:10:31 -0700

    setting up fetchDrinks

--- src/context.js
+++ src/context.js
@@ -16,6 +16,15 @@ const AppProvider = ({ children }) => {
       const data = await response.json();
       const { drinks } = data;
       if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const {
+            idDrink,
+            strDrink,
+            strDrinkThumb,
+            strAlcoholic,
+            strGlass,
+          } = item;
+        });
       } else {
         setCocktails([]);
       }
