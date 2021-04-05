ddf74fba452e6285af272ee8175952afa234767f
Joseph M <jmedina1985@hotmail.com>
2021-04-04 21:15:47 -0700

    displaying the list of cocktails

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -19,6 +19,11 @@ const CocktailList = () => {
   return (
     <section className="section">
       <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
     </section>
   );
 };
