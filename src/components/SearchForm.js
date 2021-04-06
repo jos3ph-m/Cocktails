231ba6a98a8053f9806e14475989fa606bfaf3b2
Joseph M <jmedina1985@hotmail.com>
2021-04-05 22:06:04 -0700

    search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -16,7 +16,7 @@ const SearchForm = () => {
 
   return (
     <section className="section search">
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search your favorite cocktail</label>
           <input
