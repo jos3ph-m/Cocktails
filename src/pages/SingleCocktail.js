ef96827b39d3349b86894dc3a8d6f2ca8492d698
Joseph M <jmedina1985@hotmail.com>
2021-04-07 19:44:50 -0700

    single cocktail page

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -73,8 +73,13 @@ const SingleCocktail = () => {
   } = cocktail;
   return (
     <section className="section cocktail-section">
-      <Link></Link>
+      <Link to="/" className="btn btn-primary">
+        back home
+      </Link>
       <h2 className="section-title">{name}</h2>
+      <div className="drink">
+        <img src={image} alt={name} />
+      </div>
     </section>
   );
 };
